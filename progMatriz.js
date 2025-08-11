5b1d55c2062f9e19b1dd7d508a51f6061dc1fe74
MATHEUS ORSI REIS <matheus_o_reis@estudante.sesisenai.org.br>
2025-08-11 08:02:09 -0300

    Programa de matrizes versão 1

---
+++ progMatriz.js
@@ -0,0 +1,19 @@
+let mat = [[3, 2,1 ],
+            [4, 5, 6],
+            [9, 8, 7]]
+
+console.table(mat)
+console.log('--------------------------')
+
+let mat2 = [[]]
+for(let i = 0; i < 3; i++){
+    mat2[i] = []
+    for(let j = 0; j < 3; j++){
+         mat2[i][j] = 1
+    }
+       
+}
+
+
+console.table(mat2)
+console.log('--------------------------')
